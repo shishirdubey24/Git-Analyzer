d99937bdfd83a551e90bfcf968b2e8d992b88af6
shishirdubey24 <shishirdubey221@gmail.com>
2026-02-02 13:39:39 +0530

    Upadted the file

--- Backend/Router/GitRouter.js
+++ Backend/Router/GitRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
 import { Gitclone } from "../Controllers/GitController.js";
+import { validateAndPrepare } from "../Middleware/RepoValidator.js";
+
 const router = express.Router();
-router.get("/git", Gitclone);
+
+// Chain: Validate input -> Then run Controller
+router.post("/git", validateAndPrepare, Gitclone);
+
 export default router;
