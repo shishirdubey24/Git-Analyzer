b805ea15636f2a6c47bb1c1918feaaabfd219ea3
shishirdubey24 <shishirdubey221@gmail.com>
2026-02-03 11:18:29 +0530

    Updated the structer of UI part

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import Header from "./Ui/Header";
-const App=()=>{
-return (
-<>
-     <Header/>
-</>
-)
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "./Ui/LandingPage";
+import ReportPage from "./Ui/ReportPage";
 
-}
+const App = () => {
+     return (
+          <Router>
+               <Routes>
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    <Route path="/home" element={<LandingPage />} />
+                    <Route path="/report" element={<ReportPage />} />
+               </Routes>
+          </Router>
+     );
+};
+
 export default App;
