b0c521322f87719248669d405cd0da1f22763c4b
shishirdubey24 <shishirdubey221@gmail.com>
2026-02-03 17:09:48 +0530

    Updated CORS

--- Backend/Server/Index.js
+++ Backend/Server/Index.js
@@ -1,22 +1,41 @@
 import express from "express";
-import cors from "cors"; // <--- 1. Import cors
+import cors from "cors";
 import GitRouter from "../Router/GitRouter.js";
 
 const app = express();
 
-// --- 2. Add CORS Middleware ---
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://repo-decoder.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 
-// Routing setup
 app.use("/api", GitRouter);
 
 app.get("/", (req, res) => {
-  res.send("welcome");
+  res.send("RepoLens Backend is Running");
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
